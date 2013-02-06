2e1e67540bbf0195d542d6698c8a36940816111d
revolt313 <hrouhi.kh@gmail.com>
2013-02-06 20:25:56 +0330

    renamed:    ../freq_sort.py -> freq_sort.py

--- algorithms/sorting/freq_sort.py
+++ algorithms/sorting/freq_sort.py
@@ -2,18 +2,22 @@
 	this module genarate tree of chars with it frequency,
 	this appear for some alghorithm like Huffman alghorithm
 	
-	you can use this like other modules,
+	
 
 	WARNING: this module is't work if you give bad data,
-	for more info see this http://github.com/revolt313/pyheap
+
+	for more info see http://en.wikipedia.org/wiki/Huffman_coding
 
 '''
 __version__= '1.0.0'
 
 def heap_gen(Data):
+
 	global tree,parent
 	tree=list(Data)
+
 	while len(tree)>1:
+
 		LeftChild=min(tree)
 		index=tree.index(LeftChild)
 		LeftChildIndex=list(tree[index])
@@ -24,5 +28,6 @@ def heap_gen(Data):
 		del tree[index]
 		parent=(LeftChildIndex[0]+RightChildIndex[0],LeftChild,RightChild)
 		tree.append(parent)
+
 	return tree[0]
 
